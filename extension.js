fb2b3f7907c0f3e73497f59b33d7b41ca2fb2c4f
Raphaël Rochet <raphael@rri.fr>
2022-05-24 10:38:20 +0200

    Update licence to GNU GPL v3

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
-/*  Copyright (C) 2017 Yannick Tanner
+/*  
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -12,6 +12,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (C) 2017 Yannick Tanner
+    Copyright (C) 2022 Raphaël Rochet
+
 **/
 
 const St = imports.gi.St;
